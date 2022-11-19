fabf10279c7865bf1f98f0a490a2a935c3169978
SankaraMoothi <sankaramoorthi99@gmail.com>
2022-11-19 19:36:24 +0530

    FirstCommit

---
+++ src/features/car/carSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cars: [
+    "Model S",
+    "Model Y",
+    "Model X",
+    "Model 3",
+    "Solar Panels",
+    "Solar Roofs",
+  ],
+};
+
+const carSlice = createSlice({
+  name: "car",
+  initialState,
+  reducers: {},
+});
+
+export const selectCars = (state) => state.car.cars;
+
+export default carSlice.reducer;
